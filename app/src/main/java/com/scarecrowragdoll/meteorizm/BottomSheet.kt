3af7aac641b9bef0913b163ceeec5f9120caf204
scarecrow-ragdoll <scarecrow.ragdoll@mail.ru>
2021-11-07 18:45:30 +0300

    bottom sheet

---
+++ app/src/main/java/com/scarecrowragdoll/meteorizm/BottomSheet.kt
@@ -0,0 +1,20 @@
+package com.scarecrowragdoll.meteorizm
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.bottom_sheet, container, false)
+
+    companion object {
+        const val TAG = "BottomSheet"
+    }
+}
--- app/src/main/java/com/scarecrowragdoll/meteorizm/MainActivity.kt
+++ app/src/main/java/com/scarecrowragdoll/meteorizm/MainActivity.kt
@@ -2,10 +2,30 @@ package com.scarecrowragdoll.meteorizm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.scarecrowragdoll.meteorizm.databinding.ActivityMainBinding
+import com.scarecrowragdoll.meteorizm.databinding.BottomSheetBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val bottomSheet = BottomSheet()
+        val bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.linearBottom))
+
+
+        binding.constraintHeader.setOnClickListener {
+            if (bottomSheet.isHidden) {
+                bottomSheet.show(supportFragmentManager, BottomSheet.TAG)
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
